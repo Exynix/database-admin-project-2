b26ae37dbcae411af36bed1f6c730cf797646d73
exynix <davids_barretom@javeriana.edu.co>
2025-05-11 17:49:52 -0500

    Creation of project.

---
+++ src/main/java/com/example/databaseadminproject2/DatabaseAdminProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.databaseadminproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAdminProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseAdminProject2Application.class, args);
+    }
+
+}
